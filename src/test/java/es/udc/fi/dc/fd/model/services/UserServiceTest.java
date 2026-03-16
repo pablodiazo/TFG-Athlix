190af507fe12bc0469ed5ea7198bd3db21aa969f
Juan Pablo Díaz Oíza <pablo.diazo@udc.es>
2026-03-16 19:11:07 +0100

    Añadidos test para las funcionalidades de usuario

--- src/test/java/es/udc/fi/dc/fd/model/services/UserServiceTest.java
+++ src/test/java/es/udc/fi/dc/fd/model/services/UserServiceTest.java
@@ -1,6 +1,7 @@
 package es.udc.fi.dc.fd.model.services;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import jakarta.transaction.Transactional;
 
@@ -14,6 +15,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import es.udc.fi.dc.fd.model.common.exceptions.DuplicateInstanceException;
 import es.udc.fi.dc.fd.model.common.exceptions.InstanceNotFoundException;
 import es.udc.fi.dc.fd.model.entities.Users;
+import es.udc.fi.dc.fd.model.entities.Users.RoleType;
+import es.udc.fi.dc.fd.model.services.exceptions.IncorrectLoginException;
+import es.udc.fi.dc.fd.model.services.exceptions.IncorrectPasswordException;
 
 /**
  * The Class UserServiceTest.
@@ -28,33 +32,133 @@ public class UserServiceTest {
 	@Autowired
 	private UserService userService;
 
+	private final Long NON_EXISTENT_ID = -1L;
+
 	/**
 	 * Creates the user.
 	 *
 	 * @param userName the user name
 	 * @return the user
 	 */
-	private Users createUser(String userName) {
-		return new Users(userName, "password", "firstName", "lastName", userName + "@" + userName + ".com");
+	private Users createUser(String userName, RoleType roleType) throws DuplicateInstanceException {
+		Users user = new Users(userName, "password", "firstName", "lastName", userName + "@" + userName + ".com", roleType);
+		return user;
 	}
 
-	/**
-	 * Test sign up and login from id.
-	 *
-	 * @throws DuplicateInstanceException the duplicate instance exception
-	 * @throws InstanceNotFoundException  the instance not found exception
-	 */
 	@Test
-	public void testSignUpAndLoginFromId() throws DuplicateInstanceException, InstanceNotFoundException {
+    public void testSignUpUser() throws DuplicateInstanceException, InstanceNotFoundException {
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
 
-		Users user = createUser("user");
+        Users loggedInUser = userService.loginFromId(user.getId());
+        assertEquals(user, loggedInUser);
+        assertEquals(RoleType.USER, user.getRole());
+    }
 
-		userService.signUp(user);
+	@Test
+    public void testSignUpCoach() throws DuplicateInstanceException, InstanceNotFoundException {
+        Users user = createUser("coach", RoleType.COACH);
+        userService.signUp(user);
 
-		Users loggedInUser = userService.loginFromId(user.getId());
+        Users loggedInUser = userService.loginFromId(user.getId());
+        assertEquals(user, loggedInUser);
+        assertEquals(RoleType.COACH, user.getRole());
+    }
 
-		assertEquals(user, loggedInUser);
-		assertEquals(Users.RoleType.USER, user.getRole());
+	@Test
+    public void testSignUpDuplicatedUserName() throws DuplicateInstanceException {
+        Users user1 = createUser("user", RoleType.USER);
+        userService.signUp(user1);
 
-	}
+        Users user2 = createUser("user", RoleType.USER);
+		assertThrows(DuplicateInstanceException.class, () -> userService.signUp(user2));
+    }
+
+	@Test
+    public void testLogin() throws DuplicateInstanceException, IncorrectLoginException {
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
+
+        Users loggedInUser = userService.login("user", "password");
+        assertEquals(user, loggedInUser);
+    }
+
+    @Test
+    public void testLoginWithIncorrectPassword() throws DuplicateInstanceException, IncorrectLoginException {
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
+
+        assertThrows(IncorrectLoginException.class, () -> userService.login("user", "incorrectPassword"));
+    }
+
+    @Test
+    public void testLoginWithNonExistentUserName() throws IncorrectLoginException {
+        assertThrows(IncorrectLoginException.class, () -> userService.login("nonExistentUser", "password"));
+    }
+
+	@Test
+    public void testLoginFromId() throws DuplicateInstanceException, InstanceNotFoundException {
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
+
+        Users loggedInUser = userService.loginFromId(user.getId());
+        assertEquals(user, loggedInUser);
+    }
+
+    @Test
+    public void testLoginFromIdWithNonExistentId() throws InstanceNotFoundException {
+        assertThrows(InstanceNotFoundException.class, () -> userService.loginFromId(NON_EXISTENT_ID));
+    }
+
+	@Test
+    public void testUpdateProfile() throws DuplicateInstanceException, InstanceNotFoundException {
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
+
+        String newFirstName = "newName";
+        String newLastName = "newLastName";
+        String newEmail = "newEmail@test.com";
+
+        userService.updateProfile(user.getId(), newFirstName, newLastName, newEmail);
+
+        Users updatedUser = userService.loginFromId(user.getId());
+
+        assertEquals(newFirstName, updatedUser.getFirstName());
+        assertEquals(newLastName, updatedUser.getLastName());
+        assertEquals(newEmail, updatedUser.getEmail());
+    }
+
+    @Test
+    public void testUpdateProfileWithNonExistentId() throws InstanceNotFoundException {
+        assertThrows(InstanceNotFoundException.class, () -> userService.updateProfile(NON_EXISTENT_ID, "name", "lastName", "email@test.com"));
+    }
+
+	@Test
+    public void testChangePassword() throws DuplicateInstanceException, InstanceNotFoundException,
+            IncorrectPasswordException, IncorrectLoginException {
+        
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
+
+        userService.changePassword(user.getId(), "password", "newPassword");
+
+        Users loggedInUser = userService.login("user", "newPassword");
+        assertEquals(user, loggedInUser);
+    }
+
+    @Test
+    public void testChangePasswordWithIncorrectPassword() throws DuplicateInstanceException, 
+            InstanceNotFoundException, IncorrectPasswordException {
+        
+        Users user = createUser("user", RoleType.USER);
+        userService.signUp(user);
+
+        assertThrows(IncorrectPasswordException.class, () -> userService.changePassword(user.getId(), "incorrectPassword", "newPassword"));
+    }
+
+    @Test
+    public void testChangePasswordWithNonExistentId() throws InstanceNotFoundException, IncorrectPasswordException {
+        assertThrows(InstanceNotFoundException.class, () -> userService.changePassword(NON_EXISTENT_ID, "password", "newPassword"));
+    }
+
 }
--- src/test/java/es/udc/fi/dc/fd/rest/UserControllerTest.java
+++ src/test/java/es/udc/fi/dc/fd/rest/UserControllerTest.java
@@ -1,6 +1,7 @@
 package es.udc.fi.dc.fd.rest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -14,8 +15,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.hamcrest.Matchers.is;
 
 import es.udc.fi.dc.fd.model.entities.Users;
 import es.udc.fi.dc.fd.model.entities.Users.RoleType;
@@ -23,7 +26,12 @@ import es.udc.fi.dc.fd.model.entities.UserDao;
 import es.udc.fi.dc.fd.model.services.exceptions.IncorrectLoginException;
 import es.udc.fi.dc.fd.rest.controllers.UserController;
 import es.udc.fi.dc.fd.rest.dtos.AuthenticatedUserDto;
+import es.udc.fi.dc.fd.rest.dtos.ChangePasswordParamsDto;
 import es.udc.fi.dc.fd.rest.dtos.LoginParamsDto;
+import es.udc.fi.dc.fd.rest.dtos.UserDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The Class UserControllerTest.
@@ -65,7 +73,7 @@ public class UserControllerTest {
 	private AuthenticatedUserDto createAuthenticatedUser(String userName, RoleType roleType)
 			throws IncorrectLoginException {
 
-		Users user = new Users(userName, PASSWORD, "newUser", "user", "user@test.com");
+		Users user = new Users(userName, PASSWORD, "newUser", "user", "user@test.com", RoleType.USER);
 
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		user.setRole(roleType);
@@ -79,6 +87,28 @@ public class UserControllerTest {
 		return userController.login(loginParams);
 
 	}
+
+	@Test
+    public void testSignUp_Ok() throws Exception {
+        UserDto userDto = new UserDto();
+
+        userDto.setUserName("newUserSignUp");
+        userDto.setPassword("password123");
+        userDto.setFirstName("New");
+        userDto.setLastName("User");
+        userDto.setEmail("signup@test.com");
+		userDto.setRole("USER");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        mockMvc.perform(post("/users/signUp")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(userDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.user.userName", is("newUserSignUp")));
+            }
+
+
 
 	/**
 	 * Test post login ok.
@@ -96,9 +126,117 @@ public class UserControllerTest {
 
 		ObjectMapper mapper = new ObjectMapper();
 
-		mockMvc.perform(post("/api/users/login").header("Authorization", "Bearer " + user.getServiceToken())
+		mockMvc.perform(post("/users/login").header("Authorization", "Bearer " + user.getServiceToken())
 				.contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsBytes(loginParams)))
 				.andExpect(status().isOk());
 
+	}
+
+	@Test
+	public void testPostLoginFromServiceToken_Ok() throws Exception {
+
+		AuthenticatedUserDto user = createAuthenticatedUser("tokenUser", RoleType.USER);
+
+		mockMvc.perform(post("/users/loginFromServiceToken")
+				.header("Authorization", "Bearer " + user.getServiceToken()))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+    public void testLogin_Exceptions() throws Exception {
+        LoginParamsDto params = new LoginParamsDto();
+        params.setUserName("nonExistentUser");
+        params.setPassword("pass");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        mockMvc.perform(post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(params)))
+                .andExpect(status().isNotFound());
+
+        AuthenticatedUserDto user = createAuthenticatedUser("wrongPassUser", RoleType.USER);
+        params.setUserName(user.getUserDto().getUserName());
+        params.setPassword("WrongPassword123");
+
+        mockMvc.perform(post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(params)))
+                .andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void testPutUpdateProfile_Ok() throws Exception {
+
+		AuthenticatedUserDto user = createAuthenticatedUser("updateUser", RoleType.USER);
+		AuthenticatedUserDto maliciousUser = createAuthenticatedUser("malicious", RoleType.USER);
+
+		UserDto updateDto = user.getUserDto();
+        updateDto.setFirstName("UpdatedName");
+		updateDto.setLastName("UpdatedLastName");
+		updateDto.setEmail("updated@test.com");
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		mockMvc.perform(put("/users/{id}", user.getUserDto().getId())
+                .header("Authorization", "Bearer " + user.getServiceToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is("UpdatedName")));
+
+		mockMvc.perform(put("/users/{id}", user.getUserDto().getId())
+                .header("Authorization", "Bearer " + maliciousUser.getServiceToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateDto)))
+                .andExpect(status().is4xxClientError());
+
+		ChangePasswordParamsDto passParams = new ChangePasswordParamsDto();
+        passParams.setOldPassword(PASSWORD);
+        passParams.setNewPassword("NewPass123");
+
+        mockMvc.perform(post("/users/{id}/changePassword", user.getUserDto().getId())
+                .header("Authorization", "Bearer " + maliciousUser.getServiceToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(passParams)))
+                .andExpect(status().is4xxClientError());
+	}
+
+	@Test
+	public void testPostChangePassword_Ok() throws Exception {
+
+		AuthenticatedUserDto user = createAuthenticatedUser("passUser", RoleType.USER);
+		Long userId = user.getUserDto().getId();
+
+		ChangePasswordParamsDto changePassParams = new ChangePasswordParamsDto();
+		changePassParams.setOldPassword(PASSWORD);
+		changePassParams.setNewPassword("newSafePassword");
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		mockMvc.perform(post("/users/" + userId + "/changePassword")
+				.header("Authorization", "Bearer " + user.getServiceToken())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(changePassParams)))
+				.andExpect(status().isNoContent());
+	}
+
+	@Test
+	public void testPostChangePassword_IncorrectOldPassword() throws Exception {
+
+		AuthenticatedUserDto user = createAuthenticatedUser("passUserError", RoleType.USER);
+		Long userId = user.getUserDto().getId();
+
+		ChangePasswordParamsDto changePassParams = new ChangePasswordParamsDto();
+		changePassParams.setOldPassword("wrongOldPassword");
+		changePassParams.setNewPassword("newSafePassword");
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		mockMvc.perform(post("/users/" + userId + "/changePassword")
+				.header("Authorization", "Bearer " + user.getServiceToken())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(changePassParams)))
+				.andExpect(status().is4xxClientError());
 	}
 }
